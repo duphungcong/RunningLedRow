49fe25f7d3fe4c803ad6ffa4d27a304dd5ed29b8
duphungcong <duphungcong@gmail.com>
2015-08-04 23:32:23 +0700

    HW : running led

--- RunningLED/ViewController.m
+++ RunningLED/ViewController.m
@@ -18,27 +18,119 @@
     int _numberOfBall;
     CGFloat _space; // > ball diameter;
     CGFloat _ballDiameter;
+    NSTimer* _timer; // _ indicate instant variable
+    int _lastOnLed;
+    int _lastOnLedLeft;
+    int _lastOnLedRight;
 }
 
 - (void) viewDidLoad {
     [super viewDidLoad];
     _margin = 40.0;
     _ballDiameter = 24.0;
-    [self checkSizeOfApp];
-    [self numberOfBallvsSpace];
-    [self drawRowOfBalls:14];
+    _numberOfBall = 13;
     
+    //[self checkSizeOfApp];
+    //[self numberOfBallvsSpace];
+    [self drawRowOfBalls:_numberOfBall];
+    
+    _lastOnLed = -1;
+    _lastOnLedLeft = -1;
+    _lastOnLedRight = -1;
+    //_timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(runningLedLeftToRight) userInfo:nil repeats:true];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(runningLedRightToLeft) userInfo:nil repeats:true];
+    //_timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(runningLedCenterToTwoSide) userInfo:nil repeats:true];
+    
+}
+
+- (void) runningLedLeftToRight {
+    if(_lastOnLed != -1) {
+        [self turnOffLed:_lastOnLed];
+    }
+    
+    if(_lastOnLed != _numberOfBall - 1) {
+        _lastOnLed++;
+    }
+    else {
+        _lastOnLed = 0;
+    }
+    [self turnOnLed:_lastOnLed];
+}
+
+- (void) runningLedRightToLeft {
+    if(_lastOnLed != -1) {
+        [self turnOffLed:_lastOnLed];
+    }
+    else {
+        _lastOnLed = _numberOfBall;
+    }
+    if(_lastOnLed != 0) {
+        _lastOnLed--;
+    }
+    else {
+        _lastOnLed = _numberOfBall - 1;
+    }
+    [self turnOnLed:_lastOnLed];
+}
+
+- (void) runningLedCenterToTwoSide {
+    int centerLed1, centerLed2;
+    if(_numberOfBall % 2 == 0) { // if _numberOfBall is even, led center are 02 led
+        centerLed2 = _numberOfBall / 2;
+        centerLed1 = centerLed2 - 1;
+    }
+    else { // led center id only 01 led
+        centerLed2 = _numberOfBall / 2;
+        centerLed1 = centerLed2;
+    }
+    
+    if(_lastOnLedLeft != -1 && _lastOnLedRight != -1) {
+        [self turnOffLed:_lastOnLedLeft];
+        [self turnOffLed:_lastOnLedRight];
+    }
+    else {
+        _lastOnLedLeft = centerLed1 + 1;
+        _lastOnLedRight = centerLed2 - 1;
+    }
+    
+    if(_lastOnLedLeft != 0 && _lastOnLedRight != _numberOfBall - 1) {
+        _lastOnLedLeft--;
+        _lastOnLedRight++;
+    }
+    else {
+        _lastOnLedLeft = centerLed1;
+        _lastOnLedRight = centerLed2;
+    }
+    [self turnOnLed:_lastOnLedLeft];
+    [self turnOnLed:_lastOnLedRight];
+    
+}
+
+- (void) turnOnLed: (int) index {
+    UIView* view = [self.view viewWithTag:index + 100];
+    if(view && [view isMemberOfClass:[UIImageView class]]) {
+        UIImageView* ball = (UIImageView*) view;
+        ball.image = [UIImage imageNamed:@"green"];
+    }
+}
+
+- (void) turnOffLed: (int) index {
+    UIView* view = [self.view viewWithTag:index + 100];
+    if(view && [view isMemberOfClass:[UIImageView class]]) {
+        UIImageView* ball = (UIImageView*) view;
+        ball.image = [UIImage imageNamed:@"grey"];
+    }
 }
 
 - (void) placeBallAtX: (CGFloat) x
                 andY: (CGFloat) y
              withTag: (int) tag {
-    UIImageView* ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"green"]];
+    UIImageView* ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"grey"]];
     ball.center = CGPointMake(x, y);
     ball.tag = tag;
     [self.view addSubview:ball];
     
-    NSLog(@"w = %3.0f, h = %3.0f", ball.bounds.size.width, ball.bounds.size.height);
+    //NSLog(@"w = %3.0f, h = %3.0f", ball.bounds.size.width, ball.bounds.size.height);
 }
 
 - (void) numberOfBallvsSpace {
